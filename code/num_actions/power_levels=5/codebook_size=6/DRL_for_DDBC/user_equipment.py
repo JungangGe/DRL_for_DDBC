a433118a97961ee7592b088217b30af92e9e0b0e
JungangGe <gejungang@foxmail.com>
2020-06-06 20:43:04 +0800

    Add files via upload

---
+++ code/num_actions/power_levels=5/codebook_size=6/DRL_for_DDBC/user_equipment.py
@@ -0,0 +1,15 @@
+from config import Config
+import numpy as np
+
+
+class UserEquipment:
+
+    def __init__(self, bs):
+
+        c = Config()
+
+        self.azimuth = np.random.rand() * 2 * np.pi
+        self.bs = bs
+        self.index = bs.index
+        self.distance_to_bs = np.random.rand() * (c.cell_radius - c.inner_cell_radius) + c.inner_cell_radius
+        self.location = bs.location + self.distance_to_bs * np.array([np.cos(self.azimuth), np.sin(self.azimuth)])
